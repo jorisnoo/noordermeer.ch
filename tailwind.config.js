736bf8199f34772802380d1fc5e896eb33ed5234
Joris Noordermeer <joris@noordermeer.ch>
2020-05-12 17:21:35 +0200

    New: Load Webfonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     purge: [
         './src/.vuepress/theme/**/*.vue',
     ],
     theme: {
+        fontFamily: {
+            'sans': ['"DM Sans"', ...defaultTheme.fontFamily.sans],
+            // 'serif': [...defaultTheme.fontFamily.serif],
+        },
+        colors: {
+            black: '#000000',
+            white: '#e6e6e6',
+            red: '#f9325a',
+            green: '#149646',
+            blue: '#1e50f0',
+        },
         extend: {},
     },
     variants: {},
