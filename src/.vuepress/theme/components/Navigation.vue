7980d778e910753d55bbfaf7ed5ba8f566995525
Joris Noordermeer <joris@noordermeer.ch>
2020-05-14 14:48:54 +0200

    Make navigation sticky

--- src/.vuepress/theme/components/Navigation.vue
+++ src/.vuepress/theme/components/Navigation.vue
@@ -8,14 +8,14 @@
     <nav>
         <router-link
             to="/"
-            class="round-link hover:bg-blue"
+            class="round-link hover:bg-blue lg:mb-2"
             exact-active-class="bg-blue"
         >
             Info
         </router-link>
         <router-link
             to="/work"
-            class="round-link hover:bg-green"
+            class="round-link hover:bg-green lg:mb-2"
             active-class="bg-green"
         >
             Work
--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -10,14 +10,14 @@
 </script>
 
 <template>
-    <div class="p-4 lg:flex lg:p-6">
+    <div class="p-4 lg:flex lg:p-0">
         <div class="lg:max-w-sm">
             <span class="name mb-2/9 lg:hidden">Joris</span>
             <span class="name mb-2/9 lg:hidden">Noordermeer</span>
-            <Navigation />
+            <Navigation class="lg:sticky lg:top-0 lg:p-6"/>
         </div>
-        <div class="p-1/3 mt-2/9 flex-grow lg:p-0 lg:mt-0">
-            <div class="content container lg:px-4">
+        <div class="p-1/3 lg:flex-grow mt-2/9 lg:p-6 lg:mt-0">
+            <div class="container content lg:px-4">
                 <slot><Content /></slot>
             </div>
         </div>
