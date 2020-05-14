ae45f399f6cc1f4963bcc10054ca038cec3f57fd
Joris Noordermeer <joris@noordermeer.ch>
2020-05-14 15:03:45 +0200

    Less duplicated elements

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -10,15 +10,20 @@
 </script>
 
 <template>
-    <div class="p-4 lg:flex lg:p-0">
-        <div class="lg:max-w-sm">
-            <span class="name mb-2/9 lg:hidden">Joris</span>
-            <span class="name mb-2/9 lg:hidden">Noordermeer</span>
-            <Navigation class="lg:sticky lg:top-0 lg:p-6"/>
+    <div class="p-4 lg:flex lg:py-0">
+        <div class="lg:max-w-sm lg:flex lg:flex-col-reverse lg:justify-between">
+            <div class="lg:py-6 lg:sticky lg:bottom-0">
+                <span class="name mb-2/9 lg:mb-0">Joris</span>
+                <span class="name mb-2/9 lg:hidden">Noordermeer</span>
+            </div>
+            <Navigation class="lg:sticky lg:top-0 lg:py-6"/>
         </div>
-        <div class="p-1/3 lg:flex-grow mt-2/9 lg:p-6 lg:mt-0">
+        <div class="p-1/3 mt-2/9 lg:p-6 lg:pb-0 lg:mt-0 lg:flex-grow lg:flex lg:flex-col lg:justify-between">
             <div class="container content lg:px-4">
                 <slot><Content /></slot>
+            </div>
+            <div class="hidden lg:block sticky bottom-0 pb-6">
+                <span class="name">Noordermeer</span>
             </div>
         </div>
     </div>
