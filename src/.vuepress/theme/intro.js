3835d923746e746bdd6fd6fc862969711ded6616
Joris Noordermeer <joris@noordermeer.ch>
2020-05-15 14:44:05 +0200

    Start tinkering with matterjs

---
+++ src/.vuepress/theme/intro.js
@@ -0,0 +1,130 @@
+import Matter from 'matter-js';
+import 'matter-dom-plugin';
+
+export default function intro(element) {
+
+    Matter.use('matter-dom-plugin');
+
+    var Engine = Matter.Engine,
+        Render = Matter.Render,
+        Runner = Matter.Runner,
+        World = Matter.World,
+        Bodies = Matter.Bodies,
+        Body = Matter.Body;
+    var RenderDom = Matter.RenderDom;
+    var DomBodies = Matter.DomBodies;
+    var DomBody = Matter.DomBody;
+    // var MouseConstraint = Matter.MouseConstraint;
+    var DomMouseConstraint = Matter.DomMouseConstraint;
+    var Mouse = Matter.Mouse;
+
+    // create engine
+    let engine = Engine.create({
+        timing: {
+            timeScale: 0.2,
+        },
+    });
+    let world = engine.world;
+
+
+    // create renderer
+    let render = RenderDom.create({
+        engine: engine,
+    });
+
+    RenderDom.run(render);
+
+    // create runner
+    let runner = Runner.create();
+    Runner.run(runner, engine);
+
+    var joris = DomBodies.block(200, -400, {
+        Dom: {
+            render: render,
+            element: document.querySelector('#joris'),
+        },
+        // frictionAir: 0.2,
+    });
+
+    var noordermeer = DomBodies.block(600, -400, {
+        Dom: {
+            render: render,
+            element: document.querySelector('#noordermeer'),
+        },
+        // frictionAir: 0.2,
+    });
+
+    World.add(world, [
+
+        joris,
+        noordermeer,
+
+        DomBodies.block(window.innerWidth / 2, window.innerHeight - 20, {
+            Dom: {
+                render: render,
+                element: document.querySelector('#wall-bottom'),
+            },
+            isStatic: true,
+        }),
+    ]);
+
+    // DomBody.rotate( block, -20);
+    DomBody.setAngularVelocity(joris, Math.PI / 44);
+
+    /** Mouse control **/
+    let mouse = Mouse.create(document.body);
+    let MouseConstraint = DomMouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.001,
+            render: {
+                visible: true,
+            },
+        },
+    });
+
+    World.add(world, MouseConstraint);
+
+    // keep the mouse in sync with rendering
+    render.mouse = mouse;
+
+    // engine.timing.timeScale = 0.2;
+
+    // add bodies
+    // World.add(world, [
+    //     // falling blocks
+    //     Bodies.rectangle(200, 100, 60, 60, { frictionAir: 0.001 }),
+    //     Bodies.rectangle(400, 100, 60, 60, { frictionAir: 0.05 }),
+    //     Bodies.rectangle(600, 100, 60, 60, { frictionAir: 0.1 }),
+    //
+    //     Bodies.ele
+    //
+    //     // walls
+    //     // Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+    //     // Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+    //     // Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+    //     // Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+    // ]);
+
+
+    // World.add(world);
+
+
+    // fit the render viewport to the scene
+    // Render.lookAt(render, {
+    //     min: { x: 0, y: 0 },
+    //     max: { x: 800, y: 600 }
+    // });
+
+    // context for MatterTools.Demo
+    // return {
+    //     engine: engine,
+    //     runner: runner,
+    //     render: render,
+    //     canvas: render.canvas,
+    //     stop: function() {
+    //         Matter.Render.stop(render);
+    //         Matter.Runner.stop(runner);
+    //     }
+    // };
+};
--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -1,6 +1,7 @@
 <script>
     import Navigation from "../components/Navigation";
     import Work from "../pages/Work";
+    import intro from '../intro';
 
     export default {
         name: 'Layout',
@@ -13,38 +14,58 @@
                 return this.$frontmatter.page || false;
             },
         },
+        created() {
+            // Set a random text selection color
+            let selectionColor = ['#f9325a', '#149646', '#1e50f0'][Math.floor(Math.random() * 3)];
+            document.documentElement.style.setProperty('--selection-color', selectionColor);
+        },
+        mounted() {
+            intro();
+        },
     };
 </script>
 
 <template>
-    <div class="p-4 lg:flex lg:py-0">
-        <div class="flex-col-reverse justify-between flex-shrink-0 lg:max-w-sm lg:min-h-screen lg:w-1/4 lg:flex">
-            <div class="lg:py-6 lg:sticky lg:bottom-0">
-                <span class="name mb-2/9 lg:mb-0">Joris</span>
-                <span class="name mb-2/9 lg:hidden">Noordermeer</span>
-            </div>
-            <Navigation class="lg:sticky lg:top-0 lg:py-6" />
+    <div>
+        <div class="absolute overflow-hidden inset-0 z-10 pointer-events-none">
+            <div id="debug" class="" />
+            <div id="wall-bottom" class="h-1 w-screen"></div>
+            <span id="joris" class="name">Joris</span>
+            <span id="noordermeer" class="name">Noordermeer</span>
+            <!--            <span class="name" id="webdevelopment">Web Development</span>-->
+            <!--            <span class="name block absolute w-screen" id="block2">Noordermeer</span>-->
         </div>
-        <div class="flex-col justify-between mt-2/9 pt-2 lg:p-6 lg:pb-0 lg:mt-0 lg:flex">
-            <transition
-                name="fade"
-                mode="out-in"
-            >
-                <component
-                    :is="pageComponent"
-                    v-if="pageComponent"
-                    :key="$page.key"
-                />
-                <div
-                    v-else
-                    :key="$page.key"
-                    class="container content p-1/3 lg:py-0 lg:px-4"
-                >
-                    <slot><Content /></slot>
+
+        <div class="p-4 lg:flex lg:py-0">
+            <div class="relative flex-col-reverse justify-between flex-shrink-0 lg:max-w-sm lg:min-h-screen lg:w-1/4 lg:flex">
+                <div class="lg:py-6 lg:sticky lg:bottom-0">
+                    <span class="name mb-2/9 lg:mb-0">Joris</span>
+                    <span class="name mb-2/9 lg:hidden">Noordermeer</span>
                 </div>
-            </transition>
-            <div class="sticky bottom-0 hidden pt-12 pb-6 lg:block bg-gradient-b-fade-out">
-                <span class="name">Noordermeer</span>
+                <Navigation class="lg:sticky lg:top-0 lg:py-6" />
+            </div>
+            <div class="relative flex-col justify-between mt-2/9 pt-2 lg:p-6 lg:pb-0 lg:mt-0 lg:flex">
+                <transition
+                    name="fade"
+                    mode="out-in"
+                >
+                    <component
+                        :is="pageComponent"
+                        v-if="pageComponent"
+                        :key="$page.key"
+                    />
+                    <div
+                        v-else
+                        :key="$page.key"
+                        class="container content p-1/3 lg:py-0 lg:px-4"
+                    >
+                        <slot><Content /></slot>
+                    </div>
+                </transition>
+
+                <div class="sticky bottom-0 hidden pt-12 pb-6 lg:block bg-gradient-b-fade-out">
+                    <span class="name">Noordermeer</span>
+                </div>
             </div>
         </div>
     </div>
@@ -56,6 +77,7 @@
     .fade-enter-active, .fade-leave-active {
         @apply transition-opacity duration-100 ease-in;
     }
+
     .fade-enter, .fade-leave-to {
         @apply opacity-0;
     }
