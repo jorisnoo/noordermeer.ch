eec8ca12f0c1350d0a14eed55fa689f6866bc74e
Joris Noordermeer <joris@noordermeer.ch>
2020-05-20 11:28:36 +0200

    Do not stop engine

--- src/.vuepress/theme/intro/index.js
+++ src/.vuepress/theme/intro/index.js
@@ -1,6 +1,7 @@
 import Matter from 'matter-js';
 import 'matter-dom-plugin';
 import {throttle, debounce} from 'throttle-debounce';
+import {fixMouseUpTouchEvent} from "./touchmouse";
 
 export default function runIntro(options) {
     Matter.use('matter-dom-plugin');
@@ -31,39 +32,43 @@ export default function runIntro(options) {
 
     // create runner
     let runner = Runner.create();
-    startEngine();
+    Runner.run(runner, engine);
 
-    let blockData = {};
     const calculateBlockData = () => {
-        blockData = {
+        windowHeight = window.innerHeight;
+        windowWidth = window.innerWidth;
+        return {
             joris: {
                 x: isMobile ? windowWidth * 0.3 : 32 + options.elements.joris.offsetWidth / 2,
                 y: -600,
                 rotation: Math.PI / 12,
                 collisionFilter: {category: 0x0002},
+                element: options.elements.joris,
             },
             noordermeer: {
                 x: isMobile ? windowWidth * 0.6 : document.querySelector('#right-side').offsetLeft - 32 + options.elements.noordermeer.offsetWidth / 2,
                 y: isMobile ? -500 : -800,
                 rotation: -Math.PI / 6,
                 collisionFilter: {category: 0x0002},
+                element: options.elements.noordermeer,
             },
             webDevelopment: {
                 x: windowWidth * 0.5,
                 y: -300,
                 rotation: Math.PI / 6,
                 collisionFilter: {category: 0x0004},
+                element: options.elements.webDevelopment,
             },
             wallBottom: {x: windowWidth * 0.5, y: windowHeight - 22, width: windowWidth, height: 1},
             wallLeft: {x: -1, y: windowHeight * 0.5, width: 1, height: windowHeight},
             wallRight: {x: windowWidth, y: windowHeight * 0.5, width: 1, height: windowHeight},
         };
     };
-    calculateBlockData();
+    let blockData = calculateBlockData();
 
-    const domBody = (blockData, element) => {
+    const domBody = (blockData) => {
         return DomBodies.block(blockData.x, blockData.y, {
-            Dom: {render, element},
+            Dom: {render, element: blockData.element},
             chamfer: {radius: 6},
             collisionFilter: blockData.collisionFilter,
             frictionAir: 0.1,
@@ -86,9 +91,9 @@ export default function runIntro(options) {
 
     let blocks = {
         // Add objects
-        joris: domBody(blockData.joris, options.elements.joris),
-        noordermeer: domBody(blockData.noordermeer, options.elements.noordermeer),
-        webDevelopment: domBody(blockData.webDevelopment, options.elements.webDevelopment),
+        joris: domBody(blockData.joris),
+        noordermeer: domBody(blockData.noordermeer),
+        webDevelopment: domBody(blockData.webDevelopment),
         // Add Walls
         wallBottom: wallBody(blockData.wallBottom),
         wallLeft: wallBody(blockData.wallLeft),
@@ -99,25 +104,10 @@ export default function runIntro(options) {
 
     // Add mouse control
     let mouse = Mouse.create(document.body);
-
-    mouse.element.removeEventListener('touchend', mouse.mouseup);
-    mouse.mouseup = function(event) {
-        var position = Mouse._getRelativeMousePosition(event, mouse.element, mouse.pixelRatio);
-
-        mouse.button = -1;
-        mouse.absolute.x = position.x;
-        mouse.absolute.y = position.y;
-        mouse.position.x = mouse.absolute.x * mouse.scale.x + mouse.offset.x;
-        mouse.position.y = mouse.absolute.y * mouse.scale.y + mouse.offset.y;
-        mouse.mouseupPosition.x = mouse.position.x;
-        mouse.mouseupPosition.y = mouse.position.y;
-        mouse.sourceEvents.mouseup = event;
-    };
-
-    mouse.element.addEventListener('touchend', mouse.mouseup);
+    fixMouseUpTouchEvent(mouse);
 
     let MouseConstraint = DomMouseConstraint.create(engine, {
-        mouse: mouse,
+        mouse,
         constraint: {stiffness: 0.003},
     });
     World.add(world, MouseConstraint);
@@ -134,8 +124,11 @@ export default function runIntro(options) {
     // Listen to window resize
     window.addEventListener('resize', debounce(200, resizeCanvas));
 
+    Events.on(runner, 'tick', throttle(500, checkBlockPositions));
+
 
     function checkBlockPositions() {
+
         // Remove the "Web Development" block once it fell down
         if (render.mapping.worldToView(blocks.webDevelopment.position.y) > windowHeight * 1.5) {
             Matter.Composite.remove(world, blocks.webDevelopment);
@@ -144,10 +137,10 @@ export default function runIntro(options) {
 
         // Revert the positions of the others in case the fall down
         if (!isMobile && render.mapping.worldToView(blocks.joris.position.y) > windowHeight * 3) {
-            reInsertBlock('joris');
+            reInsertBlock(blocks.joris, 'joris');
         }
         if (!isMobile && render.mapping.worldToView(blocks.noordermeer.position.y) > windowHeight * 3) {
-            reInsertBlock('noordermeer');
+            reInsertBlock(blocks.noordermeer, 'noordermeer');
         }
 
         if (!blocksHavePassedContentArea
@@ -156,12 +149,14 @@ export default function runIntro(options) {
         ) {
             blocksHavePassedContentArea = true;
             options.callbacks.end();
+
             if (!isMobile) {
                 introHasRun = true;
                 options.callbacks.endOnMobile();
-            } else {
-                removeTouchEvents();
             }
+            // else {
+            //     removeTouchEvents();
+            // }
         }
 
         if (!introHasRun
@@ -176,21 +171,22 @@ export default function runIntro(options) {
     }
 
     function resizeCanvas() {
-        if (window.innerWidth < 1024 && runner.enabled && introHasRun) {
-            stopEngine();
-            toggleMobileView();
-        } else if (window.innerWidth >= 1024 && !runner.enabled) {
-            startEngine();
-            toggleMobileView();
-            return;
-        }
+        // if (window.innerWidth < 1024 && runner.enabled && introHasRun) {
+        //     stopEngine();
+        //     toggleMobileView();
+        // } else if (window.innerWidth >= 1024 && !runner.enabled) {
+        //     startEngine();
+        //     toggleMobileView();
+        //     return;
+        // }
 
         if (window.innerWidth < 1024) {
+            toggleMobileView();
             return;
         }
 
-        // Push up the blocks on resize
         if (windowHeight > window.innerHeight) {
+            // Push up the blocks on resize
             DomBody.applyForce(blocks.joris,
                 {x: blocks.joris.position.x, y: blocks.joris.position.y},
                 {x: 0, y: -0.03},
@@ -219,8 +215,7 @@ export default function runIntro(options) {
             blocks.wallLeft, blocks.wallRight, blocks.wallBottom,
         ]);
 
-        updateWindowSize();
-        calculateBlockData();
+        blockData = calculateBlockData();
 
         blocks.wallBottom = wallBody(blockData.wallBottom);
         blocks.wallLeft = wallBody(blockData.wallLeft);
@@ -232,56 +227,62 @@ export default function runIntro(options) {
     }
 
     function toggleMobileView() {
+        updateWindowSize();
         resizeWalls();
-        reInsertBlock('joris');
-        reInsertBlock('noordermeer');
+        reInsertBlock(blocks.joris, 'joris');
+        reInsertBlock(blocks.noordermeer, 'noordermeer');
     }
 
-    function reInsertBlock(block) {
-        calculateBlockData();
-        DomBody.setPosition(blocks[block], {
-            x: render.mapping.viewToWorld(blockData[block].x),
-            y: render.mapping.viewToWorld(blockData[block].y),
-        });
-        if (blockData[block].rotation) {
-            DomBody.rotate(blocks[block], blockData[block].rotation - blocks[block].angle);
+    function reInsertBlock(block, blockKey) {
+        Matter.Composite.remove(world, block);
+        blockData[blockKey].element.style = '';
+        blockData = calculateBlockData();
+        blocks[blockKey] = domBody(blockData[blockKey]);
+        block = blocks[blockKey];
+        World.add(world, block);
+        // DomBody.setPosition(block, {
+        //     x: render.mapping.viewToWorld(blockData.x),
+        //     y: render.mapping.viewToWorld(blockData.y),
+        // });
+        if (blockData[blockKey].rotation) {
+            DomBody.rotate(block, blockData[blockKey].rotation - block.angle);
         }
     }
 
-    function stopEngine() {
-        Runner.stop(runner, engine);
-        RenderDom.stop(render);
-        runner.enabled = false;
-        removeTouchEvents();
-        // Stop tick event
-        Events.off(runner, 'tick', throttle(500, checkBlockPositions));
-    }
+    // function stopEngine() {
+    //     Runner.stop(runner, engine);
+    //     RenderDom.stop(render);
+    //     runner.enabled = false;
+    //     removeTouchEvents();
+    //     // Stop tick event
+    //     Events.off(runner, 'tick', throttle(500, checkBlockPositions));
+    // }
 
-    function startEngine() {
-        RenderDom.run(render);
-        Runner.run(runner, engine);
-        runner.enabled = true;
-        addTouchEvents();
-        // Bind tick event
-        Events.on(runner, 'tick', throttle(500, checkBlockPositions));
-    }
+    // function startEngine() {
+    //     RenderDom.run(render);
+    //     Runner.run(runner, engine);
+    //     runner.enabled = true;
+    //     addTouchEvents();
+    //     // Bind tick event
+    //     Events.on(runner, 'tick', throttle(500, checkBlockPositions));
+    // }
 
-    function addTouchEvents() {
+    // function addTouchEvents() {
         // if(mouse) {
         //     mouse.element.add('touchstart', mouse.mousedown);
         //     mouse.element.add('touchend', mouse.mouseup);
         //     mouse.element.add('touchmove', mouse.mousemove);
         // }
-    }
+    // }
 
-    function removeTouchEvents() {
+    // function removeTouchEvents() {
         // console.log('touch removed 2')
         // if (mouse) {
         //     mouse.element.removeEventListener('touchstart', mouse.mousedown);
         //     mouse.element.removeEventListener('touchend', mouse.mouseup);
             // mouse.element.removeEventListener('touchmove', mouse.mousemove);
         // }
-    }
+    // }
 }
 
 
