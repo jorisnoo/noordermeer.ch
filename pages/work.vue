204854c9b7946f76ec08e771e1e8308c2db5b52c
Joris Noordermeer <joris@noordermeer.ch>
2020-10-22 09:52:02 +0200

    Fix: Remove scroll to header

--- pages/work.vue
+++ pages/work.vue
@@ -29,15 +29,15 @@
                     : -1;
             },
         },
-        watch: {
-            openProject (openProject) {
-                if (openProject > -1) {
-                    setTimeout(() => {
-                        this.$refs.headers[this.openProject].scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
-                    }, 301);
-                }
-            },
-        },
+        // watch: {
+        // openProject (openProject) {
+        //     if (openProject > -1) {
+        //         setTimeout(() => {
+        //             this.$refs.headers[this.openProject].scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+        //         }, 301);
+        //     }
+        // },
+        // },
         created () {
             this.projectsInCurrentLocale = this.projects[this.$i18n.locale];
         },
@@ -50,38 +50,36 @@
 </script>
 
 <template>
-    <div>
+    <div class="selection-green">
         <h1 class="sr-only">
             {{ $t('work') }}
         </h1>
-        <ul class="selection-green">
-            <li
-                v-for="(project, index) in projectsInCurrentLocale"
-                :key="'project'+project.name"
+        <article
+            v-for="(project, index) in projectsInCurrentLocale"
+            :key="'project'+project.name"
+        >
+            <h2
+                :ref="'headers'"
+                class="round-link cursor-pointer"
+                :class="{
+                    'hover:bg-green': index%3 === 0,
+                    'hover:bg-red': index%3 === 1,
+                    'hover:bg-blue': index%3 === 2,
+                    'bg-green': index%3 === 0 && openProject === index,
+                    'bg-red': index%3 === 1 && openProject === index,
+                    'bg-blue': index%3 === 2 && openProject === index,
+                }"
             >
-                <h2
-                    :ref="'headers'"
-                    class="round-link cursor-pointer"
-                    :class="{
-                        'hover:bg-green': index%3 === 0,
-                        'hover:bg-red': index%3 === 1,
-                        'hover:bg-blue': index%3 === 2,
-                        'bg-green': index%3 === 0 && openProject === index,
-                        'bg-red': index%3 === 1 && openProject === index,
-                        'bg-blue': index%3 === 2 && openProject === index,
-                    }"
-                >
-                    <nuxt-link :to="localePath({ params: { slug: (openProject === index) ? null : project.slug } })">
-                        {{ project.title }}
-                    </nuxt-link>
-                </h2>
-                <slide-up-down :active="openProject === index" :duration="300">
-                    <div class="projectDescription px-1/3 pb-4 pt-2">
-                        <nuxt-content class="typo-base prose prose-base" :document="project" />
-                    </div>
-                </slide-up-down>
-            </li>
-        </ul>
+                <nuxt-link :to="localePath({ params: { slug: (openProject === index) ? null : project.slug } })">
+                    {{ project.title }}
+                </nuxt-link>
+            </h2>
+            <slide-up-down :active="openProject === index" :duration="500">
+                <div class="projectDescription px-1/3 pb-4 pt-2">
+                    <nuxt-content class="typo-base prose prose-base" :document="project" />
+                </div>
+            </slide-up-down>
+        </article>
     </div>
 </template>
 
